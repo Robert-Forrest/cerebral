b44d792db68b2ed887f9e159bf4508edce431134
Robert Forrest <robertforrest@live.com>
2022-10-26 14:15:32 +0100

    Added glass Dmax model example

---
+++ examples/Dmax.py
@@ -0,0 +1,23 @@
+import cerebral as cb
+
+cb.setup(
+    {
+        "targets": [{"name": "Dmax"}],
+        "input_features": ["mass"],
+        "data": {"files": ["data.xls"]},
+    }
+)
+
+data = cb.io.load_data(postprocess=cb.GFA.ensure_default_values_glass)
+
+model, history, train_data, test_data = cb.models.train_model(data)
+
+train_predictions = cb.models.evaluate_model(
+    model,
+    train_data["dataset"],
+    train_data["labels"],
+    test_ds=test_data["dataset"],
+    test_labels=test_data["labels"],
+    train_compositions=train_data["compositions"],
+    test_compositions=test_data["compositions"],
+)
